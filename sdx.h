02c5316f53c4d19d998633ef1ac7dad9097db2f3
BeratDizdar <139409706+BeratDizdar@users.noreply.github.com>
2026-07-11 07:56:31 +0300

    Update sdx.h

--- sdx.h
+++ sdx.h
@@ -2,5 +2,11 @@
 
 void* sdx_CreateID3D9();
 void* sdx_CreateID3DDevice9(void* d3d9, void* hwnd);
-void* sdx_CreateID3DVertexBuffer9(void* device, int size, unsigned int FVF);    // dinamik -- static ayır ya da
-void* sdx_CreateID3DIndexBuffer9(void* device, int size);                       // yoksa komple dinamik mi olsa
+void* sdx_CreateID3DVertexBuffer9(void* device, int size, void* data, unsigned int FVF, unsigned int managed);
+void* sdx_CreateID3DIndexBuffer9(void* device, int size, void* data, unsigned int managed);
+void* sdx_CreateID3DTexture9FromPath(void* device, const char* filepath);
+void* sdx_CreateRenderTargetTexture(void* device, int width, int height);
+void* sdx_CreateID3DVertexDeclaration9(void* device, const void* elements);
+void  sdx_BeginStateBlock(void* device);
+void  sdx_EndStateBlock(void* device, void** state_block);
+void  sdx_Apply(void* state_block);
